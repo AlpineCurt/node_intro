b668f30915c616bee2349873711694fc27ad0f31
Curt <alpinecurt@gmail.com>
2022-12-19 08:39:00 -0800

    complete

---
+++ step2.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const process = require('process');
+const axios = require('axios');
+
+function cat (path) {
+    fs.readFile(path, 'utf8', (err, data) => {
+        if (err) {
+            console.error(`Error reading ${path}: ${err}`);
+            process.exit(1);
+        } else {
+            console.log(data);
+        }
+    });
+}
+
+async function webCat(url) {
+    try {
+        const res = await axios.get(url);
+        console.log(res.data);
+    } catch (err) {
+        console.error(`Error getting ${url}: ${err}`);
+        process.exit(1);
+    }
+}
+
+const path = process.argv[2];
+
+if (path.startsWith('http')) {
+    webCat(path);
+} else {
+    cat(path);
+}
---
+++ step3.js
@@ -0,0 +1,54 @@
+const fs = require('fs');
+const process = require('process');
+const axios = require('axios');
+
+function cat (path, out) {
+    fs.readFile(path, 'utf8', (err, data) => {
+        if (err) {
+            console.error(`Error reading ${path}: ${err}`);
+            process.exit(1);
+        } else {
+            fileOut(data, out);
+        }
+    });
+}
+
+async function webCat(url, out) {
+    try {
+        const res = await axios.get(url);
+        fileOut(res.data, out);
+    } catch (err) {
+        console.error(`Error getting ${url}: ${err}`);
+        process.exit(1);
+    }
+}
+
+function fileOut(text, out) {
+    if (out) {
+        fs.writeFile(out, text, 'utf8', function(err) {
+            if (err) {
+                console.error(err);
+                process.exit(1);
+            }
+            console.log('File written');
+        })
+    } else {
+        console.log(text);
+    }
+}
+
+let path;
+let out;
+
+if (process.argv[2] === '--out') {
+    out = process.argv[3];
+    path = process.argv[4];
+} else {
+    path = process.argv[2];
+}
+
+if (path.startsWith('http')) {
+    webCat(path, out);
+} else {
+    cat(path, out);
+}
